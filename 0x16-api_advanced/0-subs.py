83b6225633499a536d5abdf44d01aff2e50e2ced
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-06 13:03:12 -0500

    Fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,15 @@
 """ function that contain the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """ function that queries the Reddit API and 
-    returns the number of subscribers 
-    (not active users, total subscribers) 
-    for a given subreddit. 
+    """ function that queries the Reddit API and
+    returns the number of subscribers
+    (not active users, total subscribers)
+    for a given subreddit.
     """
     response = requests.get('https://www.reddit.com/r/{}/about.json'.
-                            format(subreddit),headers={'User-Agent': 'Dioni'})
+                            format(subreddit), headers={'User-Agent': 'Dioni'})
     if response.status_code == 404:
         return 0
     return response.json()['data']['subscribers']
