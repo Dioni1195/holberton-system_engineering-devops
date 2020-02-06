afde108f3aa71f1fb190d3cc80560d6b5330ea42
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-06 15:22:45 -0500

    Improve logic

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,20 +12,15 @@ def recurse(subreddit, hot_list=[], count=0, after=""):
     if after is None:
         return hot_list
     header = {'User-Agent': 'Dioni'}
-    if len(hot_list) == 0:
-        response = requests.get('https://www.reddit.com/r/{}/hot.json'.
-                                format(subreddit), headers=header)
-    else:
-        parms = {'after': after, 'limit': count}
-        response = requests.get('https://www.reddit.com/r/{}/hot.json'.
-                                format(subreddit),
-                                headers=header, params=parms)
+    parms = {'after': after, 'limit': count}
+    response = requests.get('https://www.reddit.com/r/{}/hot.json'.
+                            format(subreddit),
+                            headers=header, params=parms)
 
     if response.status_code == 404:
         return None
-    else:
-        for hot in response.json()['data']['children']:
-            hot_list.append(hot['data']['title'])
-        after = response.json()['data']['after']
-        count = 100
-        return recurse(subreddit, hot_list, count, after)
+    for hot in response.json()['data']['children']:
+        hot_list.append(hot['data']['title'])
+    after = response.json()['data']['after']
+    count = 200
+    return recurse(subreddit, hot_list, count, after)
