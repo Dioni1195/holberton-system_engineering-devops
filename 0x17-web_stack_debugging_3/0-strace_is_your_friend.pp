912a1ed8801dac54eec84cadb5c5c6dd65d4b147
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-20 04:12:36 +0000

    First commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix a bug in an Apache config file in a WordPress container
+exec { 'fix-wordpress':
+	onlyif  => 'test -e /var/www/html/wp-settings.php',
+	command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+ 	path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
