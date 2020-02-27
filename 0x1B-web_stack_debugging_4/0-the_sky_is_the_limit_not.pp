fdec097337c634b1a646f14d22ff622aebc4e232
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-27 18:07:50 +0000

    First commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Increase number of requests allowed to Ngnix
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 15;/' /etc/nginx/nginx.conf",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
