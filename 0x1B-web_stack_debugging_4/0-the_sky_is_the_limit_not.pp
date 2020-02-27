09de9d0748128647dc95c93db22bd7a5eda21fc5
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-27 19:00:11 +0000

    Fixing to restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Increase number of requests allowed to Ngnix
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 30;/' /etc/nginx/nginx.conf",
+  command => "sed -i 's/worker_processes 4;/worker_processes 30;/' /etc/nginx/nginx.confi | service nginx restart",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+
