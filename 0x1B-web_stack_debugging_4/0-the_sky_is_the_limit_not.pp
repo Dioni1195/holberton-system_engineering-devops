193dfaab994aae3e4b9975723a53acab73b5ee64
Dioni1195 <dionisio.arango95@gmail.com>
2020-02-27 18:14:09 +0000

    Increase number of worker processes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Increase number of requests allowed to Ngnix
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 15;/' /etc/nginx/nginx.conf",
+  command => "sed -i 's/worker_processes 4;/worker_processes 30;/' /etc/nginx/nginx.conf",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
